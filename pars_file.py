fe7f59ad8ac484c36069a997b129769fc43f3e71
ivan_pc <ghhghdffgj@gmail.com>
2021-11-27 11:09:04 +0500

    upd+ full wrk parser

--- pars_file.py
+++ pars_file.py
@@ -1,66 +1,68 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL = 'https://www.braingle.com/brainteasers/category.php?category=All&fields=&value=&unrated=0&order=3&dir=0&start=0' # const
+class Parse:
+    """main pars"""
+    def __init__(self):
+        self.list_dict_riddle = []
+        self.HEADERS = {     #self. mb
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64;  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'accept': '*/*'}  # адрес браузера , которкое значение аццепта
+        self.URL = 'https://www.braingle.com/brainteasers/category.php?category=All&fields=&value=&unrated=0&order=3&dir=0&start=' # const
+        self.page_riddle_counter = 0
 
+    def get_html(self, url_page, params = None):
+        r = requests.get(url_page , headers = self.HEADERS , params = params)
+        return r
 
-HEADERS = { 'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64;  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36' , 'accept' : '*/*' } #адрес браузера , которкое значение аццепта
-
-def get_html(url_page,params = None):
-    # в параметрах должен быть пэйдж но в моём случае они измеряют в элементах лол start=45== Page 4
-    # , start 40 == page 3, start 0 == page 1 (при этом 45/40/0 не присутс на стр)
-    r = requests.get(url_page, headers = HEADERS , params = params)
-    return r
-
-def get_content(html ,list_riddle ,step = 2): #работает с данными
-    soup = BeautifulSoup(html, 'html.parser') # 2й параметр опционален,желателен , у
-    # казывает тип с которым мы работаем(суп может много:))
-    items = soup.find_all('td' ,class_ = 'nowrap col_max')
-    # if step == 1:  # смена страницы
-
-    if step == 2: # если мы на определённой странице парсим ссылки на загадки
-        items = soup.find_all('td', class_='nowrap col_max')
+    def pars_main_get_riddle_page_1(self,html):
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.find_all('td', class_='nowrap col_max')  # итемы загадок
+        # print('взял список ссылок на ')
         for item in items:
-            riddle_page(item,list_riddle)
-        # if
+            self.pars_main_get_riddle_page_2(item)
+        self.next_page()
 
-    if step == 3: # парсим загадку
+    def pars_main_get_riddle_page_2(self,item):
+        href_for_riddle = 'https://www.braingle.com/' + item.find('a').get(
+            'href')  # кусочек адреса 1 + кусочек 2 (1 статичен, 2й достаём из хтмля
+        # if self.get_html(href_for_riddle) == 200: #подмоть надо вай донт ворк
+        self.pars_riddle_page(href_for_riddle)
+        # print(href_for_riddle) # ссылка на загадку(мб её тоже в дикт?)
+
+    def pars_riddle_page(self, url):
+        html = self.get_html(url)
+        soup = BeautifulSoup(html.text, 'html.parser')
         items = soup.find_all( id = 'main_content')
         for item in items:
             riddle_name = item.find('h1').get_text()
             riddle_body = item.find_all(class_ = 'textblock')
             riddle_text,riddle_answer = riddle_body[0].get_text(),riddle_body[1].get_text()
             dict_riddle={'riddle name : ':riddle_name ,'riddle text : ':riddle_text , 'riddle answer : ':riddle_answer}
-            list_riddle.append(dict_riddle)
-            #
-    return list_riddle
+            self.list_dict_riddle.append(dict_riddle)
 
 
+    def next_page(self):
+        # print('NEXT PAGE')
+        self.page_riddle_counter += 15
+        if self.page_riddle_counter <= 15:
+            # print(self.page_riddle_counter)# принт каунтера страницы
+            self.parse_page()
+        else:
+            # print(self.list_dict_riddle)
+            print('всего распараршено загадок : ' , self.page_riddle_counter)
 
-
-def riddle_page(item,list_riddle):
-    href_for_riddle = 'https://www.braingle.com/'+ item.find('a').get('href') # кусочек адреса 1 + кусочек 2 (1 статичен, 2й достаём из хтмля
-    parse(list_riddle,3,href_for_riddle)
-    # list_riddle.append({ })
-    print(href_for_riddle)
-
-def parse(list_riddle, step = 2, url = 'https://www.braingle.com/brainteasers/category.php?category=All&fields=&value=&unrated=0&order=3&dir=0&start=0'):
-    html = get_html(url)  # возрващает страницу
-    if html.status_code == 200: # если "вэлью == 200( == True) "
-        # print(html.text) # отпринтовать страницу в тексте #BeautifulSoup metod
-        new_list = get_content(html.text ,list_riddle, step) # пердаём текст
-    else:
-        print('Error')
-    return new_list
-
-
+    def parse_page(self):
+        url = self.URL + str(self.page_riddle_counter)
+        html = self.get_html(url)
+        if html.status_code == 200 :
+            self.pars_main_get_riddle_page_1(html.text)
+        else:
+            print(' Error ')
 
 if __name__ == '__main__':
-    list_riddle = []
-    list_dict_riddle = parse(list_riddle)
-    print(list_dict_riddle)
-    for dict in list_dict_riddle:
-        for key,value in dict.items():
-            print(key , value)
-
-        print('-'*30)
+    print()
+    tst=Parse()
+    tst.parse_page()
+    for dict in tst.list_dict_riddle:
+        print(dict)
