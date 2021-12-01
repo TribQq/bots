ed57ad856d733c7cf6b6cf0297e8110a4ca55f0a
ivan_pc <ghhghdffgj@gmail.com>
2021-12-01 16:24:26 +0500

    +function +fix bug (crutch?)

--- tg_botfile_SphinxrA9.py
+++ tg_botfile_SphinxrA9.py
@@ -10,10 +10,8 @@ from telegram.utils.helpers import escape_markdown
 
 class Riddle:
     """global list for riddle"""
-    def __init__(self ): # ,head = None , body = None ,answer = None
+    def __init__(self ):
         self.l_riddle = []
-
-
 
 
 # Enable logging
@@ -24,10 +22,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
@@ -39,17 +34,11 @@ def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!_command ' )
 
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
 def gj_get_answer(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         'Да ! , это / %s / \n Молодец :) \n ну или ты просто угадал одно слово в ответе,тогда молодец, но не такой большой \n в общем поздравляю, мишка тобой доволен ʕ ᵔᴥᵔ ʔ ' %
         ridddle_user.l_riddle[2])
     ridddle_user.l_riddle = []
-
 
 
 def try_get_hint(update: Update, context: CallbackContext, inline_mode = 'off') -> None:
@@ -83,20 +72,15 @@ def riddlle_in_list(update: Update, context: CallbackContext) -> None:
 
 
 def get_riddle(update: Update, context: CallbackContext, inline_mode = 'off') -> None:
-    #работает, но чёт выглядит как говно , подумать .. потом)
     if inline_mode == 'on': # update.message.text в инлайн режиме нельзя создать, приходиться костыльно перименовывать
         upd_message = 'random'
     elif inline_mode == 'off':
         upd_message = update.message.text
-
     if 'random' in upd_message.lower() or 'случай' in upd_message.lower():
-        random_number = random.randint(0, len(shelve.open('riddle_tuples.db')))
-        # update.message.text = random.randint(0, len(shelve.open('riddle_tuples.db'))+1)
-        upd_message = random_number
-
+        upd_message = random.randint(0, len(shelve.open('riddle_tuples.db')))
     user_text = int(upd_message)
     list_riddle_len = len(shelve.open('riddle_tuples.db'))
-    if user_text in range(list_riddle_len):  # а как хранить к подсказку?м?
+    if user_text in range(list_riddle_len):
         for value in (shelve.open('riddle_tuples.db')[str(user_text)]):
             ridddle_user.l_riddle.append(value)
         message = 'Название : %s \n текст: %s' % (ridddle_user.l_riddle[0], ridddle_user.l_riddle[1])
@@ -120,81 +104,84 @@ def dont_get_riddle(update: Update, context: CallbackContext) -> None:
 
 
 def riddle_in_range(update: Update, context: CallbackContext) -> None:
-    # можно попробовать без ключевого слова а через if answer in text
-    # 1 проблема == кавычки и пр залупа
-    # 2я эту котлетку нужно раскидать по функциям, или она эволиционирует к стрёмного гомункула(уже?)
-    # 3 == инлайн режим
-    # при вводе 2й буквы ремувит на пустой лист, потов возвращет назад список на место(при 3й букве) и так по кругу...
-    #проблема в том что я не могу нормально переиспользовать этот код для инлайна т.к разные классы/комманды.
-    # ... даже если нормально заспличу по функциям , при этом он не скипает загадку и продлолжает предлагать его пока ты не жмякнешь куда либ
-    # о (например на хинт)
     if len(ridddle_user.l_riddle) != 0 :
         riddlle_in_list(update,context)
-
     else:
         dont_get_riddle(update,context)
-#создаётся, не детектит 1ю букву и чистится..
+
 
 def riddle_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Диапазон возможных номеров от 0 до %2d '%len(shelve.open('riddle_tuples.db')))
 
-
-
-def inlinequery(update: Update, context: CallbackContext) -> None:
-    """Handle the inline query."""
-    query = update.inline_query.query
-    test_img = 'https://icdn.lenta.ru/images/2021/04/27/16/20210427163138131/detail_9b31eaf4376cdff03e0ba1bcaa826a01.jpg'
-    if query == "":
-        return
-
-    if len(ridddle_user.l_riddle) == 0:
-        print(ridddle_user.l_riddle)
-        random_or_hint = InlineQueryResultArticle(
+def button_get_riddle(update: Update, context: CallbackContext) -> None:
+    gets_riddle = InlineQueryResultArticle(
             id=str(uuid4()),
             title="Random_R",
             input_message_content=InputTextMessageContent(get_riddle(update, context, 'on')),
         )
-        skip_or_cat = InlineQueryResultPhoto(
-            id=str(uuid4()),
-            photo_url = test_img,
-            thumb_url = test_img,
-            title="Cats",
-        )
-        check_answer_or_ASCII = InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="ASCII",
-            input_message_content=InputTextMessageContent(
-                '. ,_,\n(O,O)\n (   )\n-"-"-- '
-            ),
-        )
-    elif len(ridddle_user.l_riddle) != 0:
-        print(ridddle_user.l_riddle)
-        random_or_hint = InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="hint",
-            input_message_content=InputTextMessageContent(try_get_hint(update,context,'on')), # ВАЖНОЕ ПРИМЕЧАНИЕ ДЛЯ ХИНТА НУЖЕНН ДРУГОЙ ТЕКСТ В НАБОРЕ(Т.К ОН НЕ АПДЕЙТИТ ЛУЛ)
-        )
+    #возможно нужна запятая
+    return gets_riddle
 
-        # if query.strip().lower() in ridddle_user.l_riddle[2].replace('"', '').lower().split():
-        #     check_answer = 'Да ! , это / %s / \n Молодец :) \n ну или ты просто угадал одно слово в ответе,тогда молодец, но не такой большой \n в общем поздравляю, мишка тобой доволен ʕ ᵔᴥᵔ ʔ ' %ridddle_user.l_riddle[2]
-        #     ridddle_user.l_riddle = []
-        #     #поидее проблема если где и может быть то здесь , но она не уходит при комменте(
-        # else:
-        #     check_answer = 'не,не угадала'
-        check_answer_or_ASCII = InlineQueryResultArticle(
+def none_in_list_inline(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    if len(ridddle_user.l_riddle) == 0:
+        buttons = [
+            InlineQueryResultArticle(
+                id=str(uuid4()),
+                title="нету активной загадки",
+                input_message_content=InputTextMessageContent(
+                    'что бы взять нужно @ тэгнуть этого бота +ввести комманду  "get_riddle" + пробел )'
+                ),),]
+    if 'get_riddle' in query:
+        buttons = (button_get_riddle(update,context))
+    return buttons
+
+def button_check_answer(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    if query.strip().lower() in ridddle_user.l_riddle[2].replace('"', '').lower().split():
+        check_answer = 'можешь жать скип, если думаешь, что отгадала'
+    else:
+        check_answer = 'не,не такого слова в ответе нет'
+    button_check =  InlineQueryResultArticle(
             id=str(uuid4()),
             title="check answer",
-            input_message_content=InputTextMessageContent('check_answer'),
-        )
-        skip_or_cat = InlineQueryResultArticle(
+            input_message_content=InputTextMessageContent(check_answer),)
+            #пока не работающая функция ( нужно закрыть бажик сначала)
+    return button_check
+
+def button_skip_riddle(update: Update, context: CallbackContext) -> None:
+    button_skip=InlineQueryResultArticle(
             id=str(uuid4()),
             title="skip",
-            input_message_content=InputTextMessageContent(skip_riddle(update, context, 'on')),
-            # ВАЖНОЕ ПРИМЕЧАНИЕ ДЛЯ ХИНТА НУЖЕНН ДРУГОЙ ТЕКСТ В НАБОРЕ(Т.К ОН НЕ АПДЕЙТИТ ЛУЛ)
-        )
+            input_message_content=InputTextMessageContent(skip_riddle(update, context, 'on')),)
+    return button_skip
 
-    results = [
-        random_or_hint,
+def riddle_in_list_inline(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    buttons =  [
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="hint",
+            input_message_content=InputTextMessageContent(try_get_hint(update, context, 'on')),
+        ),
+    ]
+    if query in ridddle_user.l_riddle[2]: #поработать с функцией
+        buttons.append(button_check_answer(update, context))
+
+    if 'skip_riddle' in query:
+        buttons.append(button_skip_riddle(update,context))
+    return buttons
+
+def inlinequery(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    if query == "":
+        return
+    if len(ridddle_user.l_riddle) == 0:
+        buttons = none_in_list_inline(update,context)
+    elif len(ridddle_user.l_riddle) != 0:
+        buttons = riddle_in_list_inline(update, context)
+
+    all_buttons = [
         InlineQueryResultArticle(
             id=str(uuid4()),
             title="translate on en",
@@ -205,29 +192,33 @@ def inlinequery(update: Update, context: CallbackContext) -> None:
            title="translate on ru",
            input_message_content=InputTextMessageContent(ts.google(query,from_language='auto', to_language='ru')),
        ),
-        skip_or_cat,
-        check_answer_or_ASCII,
-    ]
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="ASCII",
+            input_message_content=InputTextMessageContent(
+                '. ,_,\n(O,O)\n (   )\n-"-"-- '
+            ),
+        ),]
+    for button in buttons:
+        all_buttons.append(button)
 
-    update.inline_query.answer(results)
+    update.inline_query.answer(all_buttons)
 
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("2100416145:AAE2JL23QIQ5L7ep0-zlhkqrjzlDds0E7jw")
+    updater = Updater("2100416145:AAE2K6ZGHadyqK0fsLqAG8vFGGePmCej-eE")
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("Riddle", riddle_command))
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, riddle_in_range))
-    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # on non command i.e message - echo the message on Telegram
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, riddle_in_range))
+
+
     dispatcher.add_handler(InlineQueryHandler(inlinequery))
 
     # Start the Bot
@@ -240,7 +231,7 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    #пользователей заносить в бд как?
+    #пользователей заносить в бд ?
+    print('check restart')
     ridddle_user = Riddle()
     main()
-
