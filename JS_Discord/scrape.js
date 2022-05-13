cbecc430d2f29ec24a6efc65a21197412e47b404
TribQq <ghhghdffgj@gmail.com>
2022-05-13 22:52:43 +0500

    Queuing system

--- JS_Discord/scrape.js
+++ JS_Discord/scrape.js
@@ -3,36 +3,31 @@ const ytdl = require('ytdl-core');
 const streams = require('stream');
 const fs = require('fs');
 const { createAudioResource, StreamType } = require('@discordjs/voice');
-
 var expression = /\/watch\?v=(\w+)/gi;
 var rgx = new RegExp(expression);
-
 async function getLink(query){
     const resp = await axios.get(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
-
     let arr = [];
     while ((result = rgx.exec(resp.data))){
         arr.push(result);
     }
-
     // console.log(arr);
 
     for (let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
+        // console.log(arr[i]);
         let link = `https://www.youtube.com/watch?v=${arr[i][1]}`;
         return link;
 
     }
-
     return undefined;
- 
+
 }
 
-module.exports = async (query, interaction) => {
+async function playSound(link){
     var ps = new streams.PassThrough();
-    let link = await getLink(query);
+
     if (link){
-        await interaction.followUp(`Playing: ${link}`);
+        // await interaction.followUp(`Playing: ${link}`);
         ytdl(link, {
             quality: 'highestaudio',
             filter: format => format.container === "webm",
@@ -46,5 +41,11 @@ module.exports = async (query, interaction) => {
         // rsc = createAudioResource("./test.mp3");
         return rsc;
     }
+
     return undefined;
+}
+
+module.exports = {
+    "getLink": getLink,
+    "playSound": playSound
 }
